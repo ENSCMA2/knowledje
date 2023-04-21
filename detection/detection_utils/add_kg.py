899d6e69c58c2f24d8c3d1dad16a654ccba051ba
Karina Halevy (she/her/hers) <karina.halevy@gmail.com>
2023-04-21 15:18:00 -0400

    comment and change file paths for add_kg

--- detection/detection_utils/add_kg.py
+++ detection/detection_utils/add_kg.py
@@ -1,9 +1,14 @@
+'''
+Utilities to augment a dataset with knowledge graph context
+'''
+
 import csv
 import nltk
 import json
 nltk.download("punkt")
 from nltk.tokenize import word_tokenize
 
+# given a word, returns all words within edit distance of 1
 def edits1(word):
     letters = 'abcdefghijklmnopqrstuvwxyz'
     splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]  
@@ -13,18 +18,23 @@ def edits1(word):
     inserts = [L + c + R for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts) 
 
+# apply edits1 to a list of words
 def editsl1(words):
     return set.union(*[edits1(w) for w in words])
 
+# same as edits1 but for edit distance 2
 def edits2(word):
     return set(e2 for e1 in edits1(word) for e2 in edits1(e1))
 
+# apply edits2 to a list of words
 def editsl2(words):
     return set.union(*[edits2(w) for w in words])
 
+# return both edits1 and edits2 of a list of words
 def editslu2(words):
     return set.union(editsl1(words), editsl2(words))
 
+# same as above 3 functions but for edit distance 3
 def edits3(word):
     return set(e3 for e2 in edits2(word) for e3 in edits1(e2))
 
@@ -34,6 +44,7 @@ def editsl3(words):
 def editslu3(words):
     return set.union(editslu2(words), editsl3(words))
 
+# for edit distance 4
 def edits4(word):
     return set(e4 for e3 in edits3(word) for e4 in edits1(e3))
 
@@ -43,6 +54,7 @@ def editsl4(words):
 def editslu4(words):
     return set.union(editslu3(words), editsl4(words))
 
+# for edit distance 5
 def edits5(word):
     return set(e5 for e4 in edits4(word) for e5 in edits1(e4))
 
@@ -52,10 +64,12 @@ def editsl5(words):
 def editslu5(words):
    return set.union(editslu4(words), editsl5(words))
 
+# get phrases to look up in knowledge graph from a given social media post
 def get_ngrams(post):
     unigrams = word_tokenize(post)
     bigrams = [unigrams[i] + " " + unigrams[i + 1] for i in range(len(unigrams) - 1)]
     trigrams = [unigrams[i] + " " + unigrams[i + 1] + " " + unigrams[i + 2] for i in range(len(unigrams) - 2)]
+    # uncomment as desired to get more ngrams and edits
     # fourgrams = [" ".join(unigrams[i:i + 4]) for i in range(len(unigrams) - 3)]
     # fivegrams = [" ".join(unigrams[i:i + 5]) for i in range(len(unigrams) - 4)]
     allgrams = set.union(set(unigrams), # editsl1(unigrams) if len(unigrams) > 0 else set(), 
@@ -65,11 +79,14 @@ def get_ngrams(post):
                          # fivegrams, editslu5(fivegrams))
     return allgrams
 
+# get complete context based on knowledge graph lookup for a post
 def get_context(post, kg):
     ngrams = get_ngrams(post)
     context = ""
     with open(kg, "rb") as kg_file:
         knowledge = json.load(kg_file)
+    # for each ngram in the post, look it up in the kg and add relevant context 
+    # based on type of kg entry
     for ngram in ngrams:
         try:
             info = knowledge[ngram.lower()]
@@ -115,13 +132,13 @@ def get_context(post, kg):
                 print("forgot to account for", info["type"])
         except:
             continue
-    if len(context) > 0 and len(context) < 1500:
-        print(context)
     return context
 
+# augment a post with its knowledge graph context, format it in a BERT-compatible way
 def get_augmented_post(post, kg):
     return get_context(post, kg) + "[SEP]  " + post
 
+# augment an entire dataset with knowledge graph context
 def get_final_dataset(data_file, kg, out_file, id_first = False):
     with open(data_file, "r+") as o:
         rows = list(csv.reader(o, delimiter = "\t"))
@@ -143,6 +160,7 @@ def get_final_dataset(data_file, kg, out_file, id_first = False):
                 else:
                     writer.writerow([augmented_posts[i], labels[i]])
 
-get_final_dataset("/n/home04/khalevy/hate_speech_detection/data/post_level/echo_posts_2_labels.tsv", 
-                  "/n/home04/khalevy/hate_speech_detection/data/post_level/kg.json",
-                  "/n/home04/khalevy/hate_speech_detection/data/post_level/echo_kg_sep.tsv", False)
+# change file paths as necessary
+get_final_dataset("../../data/echo_posts_2_labels.tsv", 
+                  "../../data/kg.json",
+                  "../../data/echo_kg_sep.tsv", False)
